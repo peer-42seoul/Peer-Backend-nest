af89d04c43aaddbe3338463bf9151c2694e82947
haryu <haryu@student.42seoul.kr>
2023-11-19 04:27:36 +0900

    FEAT: alram module maked

---
+++ src/alram/alram.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('alram')
+export class AlramController {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlramController } from './alram/alram.controller';
+import { AlramService } from './alram/alram.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AlramController],
+  providers: [AppService, AlramService],
 })
 export class AppModule {}
