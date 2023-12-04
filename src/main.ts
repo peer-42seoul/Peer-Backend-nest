c66444724cdea7a8506172258177c9a17c4685ae
haryu <ryu.axel@gmail.com>
2023-12-04 16:06:00 +0900

    FEAT: add CORS settings

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   console.log(process.env.SERVER_PORT);
+  app.enableCors({
+    origin: process.env.FRONT_URL
+  });
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
