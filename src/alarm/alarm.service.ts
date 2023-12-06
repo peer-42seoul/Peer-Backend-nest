b4eb74d4875bb6f3cdd834bd7c1fa49ea9be5280
haryu <ryu.axel@gmail.com>
2023-12-06 18:03:55 +0900

    WIP

--- src/alarm/alarm.service.ts
+++ src/alarm/alarm.service.ts
@@ -18,14 +18,41 @@ export class AlarmService {
     return this.tempRepository.find();
   }
 
-  public async sendPushNoti(token: string, title: string, body: string): Promise<void> {
+  // public async sendPushNoti(token: string, title: string, body: string): Promise<void> {
+  //   const message = {
+  //     notification: {
+  //       title: title,
+  //       body: body,
+  //     },
+  //     token: token,
+  //   }
+  //   try {
+  //     await admin.messaging().send(message);
+  //     this.logger.log('message send sucessfully');
+  //   } catch (error) {
+  //     this.logger.error('send message is faild', error);
+  //   }
+  // }
+    public async sendPushNoti(token: string, title: string, body: string): Promise<void> {
+      const icon =  "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1567593192/noticon/za5oft8gpi5yabrlvgfp.gif";
+      const click = "https://www.peer-test.co.kr";
     const message = {
+      webpush: {
       notification: {
         title: title,
         body: body,
+        icon: icon,
+        click_action: click,
+        data: {
+          link: "https://www.peer-test.co.kr"
+        },
       },
+      headers: {
+        Urgency: "High", 
+      },
+    },
       token: token,
-    }
+    };
     try {
       await admin.messaging().send(message);
       this.logger.log('message send sucessfully');
